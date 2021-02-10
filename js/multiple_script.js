55a85064faaf68ce044816c7b33d9f912c0a3ed7
Michele Sousa <21959@student.dorset-college.ie>
2021-02-10 08:46:10 +0000

    commit

--- js/multiple_script.js
+++ js/multiple_script.js
@@ -1,5 +1,5 @@
 function Restaurant (name, tables, booked){
-this.name = name;
+    this.name = name;
     this.tables = tables;
     this.booked = booked;
     this.checkAvailablity = function() {
@@ -10,14 +10,14 @@ this.name = name;
 var yellowRestaurant = new Restaurant('Yellow', 30, 18);
 var greenRestaurant = new Restaurant('Green', 52, 15);
 
-var details1 = yellowRestaurant.name + 'tables: ';
+var details1 = yellowRestaurant.name + ' tables: ';
 
 details1 += yellowRestaurant.checkAvailablity();
 
 var elRest1 = document.getElementById('rest1');
 elRest1.textContent = details1;
-add
-var details2 = greenRestaurant.name + 'tables: ';
+
+var details2 = greenRestaurant.name + ' tables: ';
 details2 += greenRestaurant.checkAvailablity();
 
 var elRest2 = document.getElementById('rest2');

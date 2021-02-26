b7cdbf75cf4664bc1c31e898b3cd0fb0a615a8f2
Michele Sousa <21959@student.dorset-college.ie>
2021-02-26 12:37:01 +0000

    commit

--- js/object_constructor.js
+++
@@ -1,9 +0,0 @@
-var restaurant = new Object();
-
-restaurant.name = 'Yellow';
-restaurant.tables = 20;
-restaurant.booked = 15;
-restaurant.checkAvailablity = function(){
-    return this.tables - tables.booked;
-};
-
